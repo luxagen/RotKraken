47adb76fb6d8bfc479ef4face7d0726c0e1131f5
luxagen <hello@luxagen.com>
2022-01-17 10:33:22 +0000

    rkdiff: better error handling

--- rkdiff.sh
+++ rkdiff.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-	echo "Exactly two arguments (paths to compare) should be passed!"
+	echo "Exactly two arguments (paths to compare) required"
 	exit 1
 fi
+
+if [ ! -e "$1" ]; then echo "Directory not found: '$1'"; fi
+if [ ! -e "$2" ]; then echo "Directory not found: '$2'"; fi
+if [ ! -e "$1" ] || [ ! -e "$2" ]; then exit 2; fi
+
+set -e
 
 rke "$1" | rkdiff-stdin "$2"
